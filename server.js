1ba0158f09a61d09fe0aafb123801bc3cd354003
Asier MD <asier.md09@gmail.com>
2024-05-10 15:35:44 +0200

    backend hecho

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require("json-server")
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+const allowAccessFromAnywhere = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    next();
+}
+server.use(allowAccessFromAnywhere)
+const router = jsonServer.router("db.json")
+server.use(router)
+const PORT = 5005
+server.listen(PORT, () =>{
+    console.log(`Server online, puerto: ${PORT}`)
+    console.log(`Acceso local en http://localhost:${PORT}`)
+})
